7ee90a21437093a7f4887d44610a426b6bbe41ba
kavya-chaturvedi <70815337+kavya-chaturvedi@users.noreply.github.com>
2021-01-18 01:04:00 +0530

    Caclculator App in order to practice Components of React

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import PI from "./Math";
-import { doublePI, triplePI } from "./Math";
+//===Importing different functions except default==
+//import PI, {doublePI, triplePI } from "./Math";
+
+//==Importing all at once using wild card===
+import * as PI from "./Math";
 
 ReactDOM.render(
   <ul>
-    <li>{PI}</li>
-    <li>{doublePI()}</li>
-    <li>{triplePI()}</li>
+    <li>{PI.default}</li>
+    <li>{PI.doublePI()}</li>
+    <li>{PI.triplePI()}</li>
   </ul>,
   document.getElementById("root")
 );
